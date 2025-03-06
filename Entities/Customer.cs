79324758fd57b9938b78a68ce418af3c6ea07b85
SofiaNords <sofia.k.nordstrom@gmail.com>
2025-03-06 08:01:12 +0100

    Add project files.

---
+++ Entities/Customer.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace EStoreAPI.Entities
+{
+    public class Customer
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [MaxLength(100)]
+        public string Email { get; set; }
+
+        [Required]
+        [Phone]
+        public string Mobile { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Street { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string City { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string PostalCode { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Country { get; set; }
+    }
+}
---
+++ Entities/Product.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EStoreAPI.Entities
+{
+    public class Product
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public int ProductNumber { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [MaxLength(200)]
+        public string Description { get; set; }
+
+        [Range(1.00, double.MaxValue, ErrorMessage = "Price must be greater than 0")]
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Category { get; set; }
+
+        public bool IsDiscontinued { get; set; } = false;
+    }
+}
