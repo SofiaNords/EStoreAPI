4c05673af23f232a9aa3cc492f30af81110d3a81
SofiaNords <sofia.k.nordstrom@gmail.com>
2025-03-07 13:38:37 +0100

    Create IOrderRepository & OrderRepository - Register in DI Program.cs

---
+++ Services/IOrderRepository.cs
@@ -0,0 +1,17 @@
+﻿using EStoreAPI.Entities;
+
+namespace EStoreAPI.Services
+{
+    public interface IOrderRepository
+    {
+        public interface IOrderRepository
+        {
+            Task<Order?> GetOrderAsync(string orderId);
+            Task<IEnumerable<Order>> GetOrdersByCustomerAsync(string customerId);
+            Task AddOrderAsync(Order order);
+            Task UpdateOrderAsync(Order order);
+            Task DeleteOrderAsync(string orderId);
+        }
+
+    }
+}
---
+++ Services/OrderRepository.cs
@@ -0,0 +1,47 @@
+﻿using EStoreAPI.Entities;
+using MongoDB.Driver;
+
+namespace EStoreAPI.Services
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly IMongoCollection<Order> _orderCollection;
+
+        public OrderRepository(IMongoDatabase database)
+        {
+            _orderCollection = database.GetCollection<Order>("orders");
+        }
+
+        public async Task<Order?> GetOrderAsync(string orderId)
+        {
+            return await _orderCollection
+                .Find(o => o.Id == orderId)
+                .FirstOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<Order>> GetOrdersByCustomerAsync(string customerId)
+        {
+            return await _orderCollection
+                .Find(o => o.CustomerId == customerId)
+                .ToListAsync();
+        }
+
+        public async Task AddOrderAsync(Order order)
+        {
+            await _orderCollection.InsertOneAsync(order);
+        }
+
+        public async Task UpdateOrderAsync(Order order)
+        {
+            var filter = Builders<Order>.Filter.Eq(o => o.Id, order.Id);
+            await _orderCollection.ReplaceOneAsync(filter, order);
+        }
+
+        public async Task DeleteOrderAsync(string orderId)
+        {
+            var filter = Builders<Order>.Filter.Eq(o => o.Id, orderId);
+            await _orderCollection.DeleteOneAsync(filter);
+        }
+
+    }
+}
