2dcc0b203ddc1370b931536988622b06ca89216c
SofiaNords <sofia.k.nordstrom@gmail.com>
2025-03-07 13:28:08 +0100

    Create Order & OrderItem entities

---
+++ Entities/Order.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System.ComponentModel.DataAnnotations;
+
+namespace EStoreAPI.Entities
+{
+    public class Order
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        [BsonRequired]
+        public string CustomerId { get; set; }
+
+        [BsonRequired]
+        public List<OrderItem> Items { get; set; }
+
+    }
+}
