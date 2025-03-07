95c782e526370ba654db6f1b56da37cd7d4da20a
SofiaNords <sofia.k.nordstrom@gmail.com>
2025-03-07 13:50:05 +0100

    Create OrderProfile

---
+++ Profiles/OrderProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+
+namespace EStoreAPI.Profiles
+{
+    public class OrderProfile : Profile
+    {
+        public OrderProfile()
+        {
+            CreateMap<Entities.Order, Models.OrderDto>();
+        }
+        
+    }
+}
