1b90bbab82394066ebba6f12c5d01047d02605f3
SofiaNords <sofia.k.nordstrom@gmail.com>
2025-03-07 14:24:05 +0100

    Create OrderItemForCreationDto

---
+++ Models/OrderItemForCreationDto.cs
@@ -0,0 +1,9 @@
+﻿namespace EStoreAPI.Models
+{
+    public class OrderItemForCreationDto
+    {
+        public string ProductId { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+    }
+}
