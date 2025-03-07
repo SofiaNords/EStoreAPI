463d2bcd192c5c53afa5f81b5f1db7a171c67897
SofiaNords <sofia.k.nordstrom@gmail.com>
2025-03-07 12:23:37 +0100

    Create ProductForCreationDto

---
+++ Models/ProductForCreationDto.cs
@@ -0,0 +1,17 @@
+﻿namespace EStoreAPI.Models
+{
+    public class ProductForCreationDto
+    {
+        public string ProductNumber { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Category { get; set; }
+
+        public bool IsDiscontinued { get; set; }
+    }
+}
