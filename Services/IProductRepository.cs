14f483a3ee79fa4da16cdb71f2847db7d684f9b5
SofiaNords <sofia.k.nordstrom@gmail.com>
2025-03-07 09:30:34 +0100

    Create IProductRepository

---
+++ Services/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using EStoreAPI.Entities;
+
+namespace EStoreAPI.Services
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetAllCustomersAsync();
+
+        Task<Product?> GetCustomerAsync(string productId);
+        Task AddCustomerAsync(Product product);
+
+        Task DeleteCustomerAsync(string productId);
+
+        Task UpdateCustomerAsync(Product product);
+    }
+}
