3d2bc7ae44b51317609e0d5ceb3d44758427bcf9
SofiaNords <sofia.k.nordstrom@gmail.com>
2025-03-26 13:07:54 +0100

    Update OrderItemForCreationDto with required, max length etc.

--- Models/OrderItemForCreationDto.cs
+++ Models/OrderItemForCreationDto.cs
@@ -1,9 +1,18 @@
-﻿namespace EStoreAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EStoreAPI.Models
 {
     public class OrderItemForCreationDto
     {
+        [Required(ErrorMessage = "Produkt är obligatorisk")]
         public string ProductId { get; set; }
+
+        [Required(ErrorMessage = "Antal är obligatoriskt")]
+        [Range(1, int.MaxValue, ErrorMessage = "Antalet måste vara större än 0")]
         public int Quantity { get; set; }
+
+        [Required(ErrorMessage = "Pris är obligatoriskt")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Pris måste vara större än 0")]
         public decimal Price { get; set; }
     }
 }
