e8f9c64a8506b971ce2e53b20c5efb177136b394
SofiaNords <sofia.k.nordstrom@gmail.com>
2025-03-26 13:05:50 +0100

    Update OrderForCreationDto with required, max lengt etc.

--- Models/OrderForCreationDto.cs
+++ Models/OrderForCreationDto.cs
@@ -1,9 +1,18 @@
-﻿namespace EStoreAPI.Models
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace EStoreAPI.Models
 {
     public class OrderForCreationDto
     {
+        [Required(ErrorMessage = "Kund är obligatorisk")]
         public string CustomerId { get; set; }
+
+        [Required(ErrorMessage = "Ordern måste innehålla minst en produkt")]
         public List<OrderItemForCreationDto> Items { get; set; }
+
+        [Required(ErrorMessage = "Orderdatum är obligatoriskt")]
         public DateTime OrderDate { get; set; }
     }
 }
