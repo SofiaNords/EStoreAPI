589a9d3a7e3522b61bd92cea0fb832156191f686
SofiaNords <sofia.k.nordstrom@gmail.com>
2025-03-27 09:01:28 +0100

    Extend XML documentation in OrderController

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -23,11 +23,11 @@ namespace EStoreAPI.Controllers
         /// Get all orders
         /// </summary>
         /// <response code="200">
-        /// Returns a list with orders
+        /// Returns a list of orders. If no orders are found, returns an empty list.
         /// </response>
+        /// <response code="500">Internal server error</response>
         [HttpGet]
         [ProducesResponseType(typeof(IEnumerable<OrderDto>), 200)]
-        [ProducesResponseType(404)]
         [ProducesResponseType(500)]
         public async Task<ActionResult<IEnumerable<OrderDto>>> GetAllOrders()
         {
@@ -35,7 +35,7 @@ namespace EStoreAPI.Controllers
 
             if (orders == null || !orders.Any())
             {
-                return NotFound();
+                return Ok(new List<OrderDto>());
             }
 
             var orderDto = _mapper.Map<IEnumerable<OrderDto>>(orders);
@@ -45,10 +45,12 @@ namespace EStoreAPI.Controllers
 
 
         /// <summary>
-        /// Get a specifik order by id
+        /// Get a specific order by id
         /// </summary>
-        /// <param name="id"></param>
-        /// <response code="200">Returns a specifik order by id</response>
+        /// <param name="id">The unique identifier of the order</param>
+        /// <response code="200">Returns the order details</response>
+        /// <response code="404">If no order with the specified id is found</response>
+        /// <response code="500">Internal server error</response>
         [HttpGet("{id}")]
         [ProducesResponseType(typeof(OrderDto), 200)]
         [ProducesResponseType(404)]
@@ -72,7 +74,8 @@ namespace EStoreAPI.Controllers
         /// Add a new order
         /// </summary>
         /// <param name="orderForCreationDto">The order information to create</param>
-        /// <response code="201">Returns the created order</response>
+        /// <response code="201">Returns the created order with a location header to access the newly created order</response>
+        /// <response code="500">Internal server error</response>
         [HttpPost]
         [ProducesResponseType(typeof(OrderDto), 201)]
         [ProducesResponseType(500)]
@@ -80,7 +83,14 @@ namespace EStoreAPI.Controllers
         {
             var order = _mapper.Map<Order>(orderForCreationDto);
 
-            await _orderRepository.AddOrderAsync(order);
+            try
+            {
+                await _orderRepository.AddOrderAsync(order);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "An error occurred while creating the order.");
+            }
 
             var orderDto = _mapper.Map<OrderDto>(order);
 
